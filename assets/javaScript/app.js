1855ab73b2e8fe634715a4867122e5330bd21997
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-15 22:06:08 -0400

    moved firebase link to app js file

---
+++ assets/javaScript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCoxt6f4BnTujkxsuqb5SKyHlFjGtMZeKM",
+  authDomain: "train-scheduler-6aaa4.firebaseapp.com",
+  databaseURL: "https://train-scheduler-6aaa4.firebaseio.com",
+  projectId: "train-scheduler-6aaa4",
+  storageBucket: "train-scheduler-6aaa4.appspot.com",
+  messagingSenderId: "447891290048",
+  appId: "1:447891290048:web:e31b57bff2e0613f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
